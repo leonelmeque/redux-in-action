cf20b9e9ce326a8e6a423e458bdbf0b95731c73d
Leonel Meque <leomeque@gmail.com>
2022-04-21 22:44:40 +0200

    feat: add async redux action creator to fetch todos

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,14 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
+import thunk from 'redux-thunk'
 import tasks from './reducers/tasks-reducer'
 
 const reducers = combineReducers({
-    tasks
+    tasks,
 })
 
 const store = configureStore({
     reducer: reducers,
-    devTools: true
+    devTools: true,
+    middleware:[thunk]
 })
 
 export default store
